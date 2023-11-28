91985584c2dfe58d99ca624e6b6cb63bf6b59909
Rob Casey <rcasey@gmail.com>
2023-11-28 13:14:43 +1100

    Updated ASN.1 parsing to provide more ubiquitous parsing

--- ngx_http_est.c
+++ ngx_http_est.c
@@ -12,7 +12,7 @@
 #include "ngx_http_est.h"
 
 
-static int ngx_http_est_asn1_parse(ngx_conf_t *cf, const unsigned char *data, size_t length, void *conf);
+static int ngx_http_est_asn1_parse(ngx_array_t *array, const unsigned char **data, size_t length, off_t offset);
 
 static char * ngx_http_est_command_csr_attrs(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 
@@ -155,71 +155,103 @@ ngx_module_t ngx_http_est_module = {
 };
 
 
-static int 
-ngx_http_est_asn1_parse(ngx_conf_t *cf, const unsigned char *data, size_t length, void *conf) {
-    ngx_http_est_loc_conf_t *lcf = conf;
-    ngx_str_t *attribute;
+static int
+ngx_http_est_asn1_parse(ngx_array_t *array, const unsigned char **data, size_t length, off_t offset) {
     ASN1_OBJECT *obj;
-    const unsigned char *p1, *p2;
+    ngx_str_t *attribute;
+    const unsigned char *end, *p1, *p2, *p3, *p4;
     char buf[128];
-    long c, len;
-    int class, nid, tag;
-    int ret, val;
+    int class, header, ret, tag, val;
+    long len;
 
-    ret = -1;
-    p1 = data;
-    c = length;
+    /*
+        This function is intended to parse through an ASN.1 structure, identifying 
+        object identifiers contained therein. This function has been drawn from the 
+        libest library within which the corresponding function has been derived from 
+        the OpenSSL library. 
+    */
 
-    while (c > 0) {
-        p2 = p1; 
-        val = ASN1_get_object(&p1, &len, &tag, &class, c);
+    /* assert(array != NULL); */
+
+    obj = NULL;
+    p1 = *data;
+    end = p1 + length;
+    p2 = p1 - 1;
+
+    while ((p1 < end) && (p2 < p1)) {
+        p2 = p1;
+        val = ASN1_get_object(&p1, &len, &tag, &class, length);
         if (val & 0x80) {
-            goto error;
+            *data = p1;
+            return -1;
         }
+        header = p1 - p2;
+        length -= header;
 
-        switch (tag) {
-            case V_ASN1_OBJECT:
-                obj = d2i_ASN1_OBJECT(NULL, &p2, c);
-                if (obj == NULL) {
-                    goto error;
+        if (val & V_ASN1_CONSTRUCTED) {
+            p3 = p1 + len;
+            if ((size_t)len > length) {
+                *data = p1;
+                return 0;
+            }
+            if ((val == 0x21) && 
+                    (len == 0)) {
+                ret = ngx_http_est_asn1_parse(array, &p1, (size_t)(end - p1), offset + (p1 - *data));
+                if (ret == 0) { 
+                    *data = p1;
+                    return 0;
                 }
-                p1 = p2;
-
-                nid = OBJ_obj2nid(obj);
-                if (nid == NID_pkcs9_challengePassword) {
-                    ASN1_OBJECT_free(obj);
+                if (p1 >= end) {
                     break;
                 }
-
+            } else {
+                while (p1 < p3) {
+                    ret = ngx_http_est_asn1_parse(array, &p1, (size_t)len, offset + (p1 - *data));
+                    if (ret == 0) { 
+                        *data = p1;
+                        return 0;
+                    }
+                }
+            }
+        }
+        else if (class != 0) {
+            p1 += len;
+        }
+        else {
+            if (tag == V_ASN1_OBJECT) {
+                p4 = p2;
+                if ((obj = d2i_ASN1_OBJECT(NULL, &p4, len + header)) == NULL) {
+                    *data = p1;
+                    return 0;
+                }
                 i2t_ASN1_OBJECT(buf, sizeof(buf), obj);
-                /* assert(lcf != NULL); */
-                /* assert(lcf->attributes != NULL); */
-                attribute = ngx_array_push(lcf->attributes);
+                /* assert(strlen(buf) > 0); */
+                attribute = ngx_array_push(array);
                 if (attribute == NULL) {
                     goto error;
                 }
-                attribute->len = strlen(buf);
-                attribute->data = ngx_pnalloc(cf->pool, attribute->len);
+                attribute->len = strlen(buf) + 1;
+                attribute->data = ngx_pnalloc(array->pool, attribute->len);
                 if (attribute->data == NULL) {
                     goto error;
                 }
                 ASN1_OBJECT_free(obj);
-                break;
-
-            case V_ASN1_SEQUENCE:
-            case V_ASN1_SET:
-                break;
-
-            default:
-                p1 += len;
-                break;
+            }
+            p1 += len;
+            if ((tag == V_ASN1_EOC) &&
+                    (class == 0)) {
+                *data = p1;
+                return 0;
+            }
         }
-        c -= (p1 - data);
+        length -= len;
     }
+    *data = p1;
+    return 0;
 
-    ret = 0;
 error:
-    return ret;
+    ASN1_OBJECT_free(obj);
+    return -1;
 }
 
 
@@ -229,7 +261,7 @@ ngx_http_est_command_csr_attrs(ngx_conf_t *cf, ngx_command_t *cmd, void *conf) {
     BIO *in;
     BUF_MEM *buf;
     size_t length;
-    char *rv;
+    char *pp, *rv;
     int ret;
 
     rv = ngx_conf_set_str_slot(cf, cmd, conf);
@@ -276,7 +308,8 @@ ngx_http_est_command_csr_attrs(ngx_conf_t *cf, ngx_command_t *cmd, void *conf) {
                 goto error;
             }
         }
-        if (ngx_http_est_asn1_parse(cf, (const unsigned char *) buf->data, length, conf) != 0) {
+        pp = buf->data;
+        if (ngx_http_est_asn1_parse(lcf->attributes, (const unsigned char **) &pp, length, 0) != 0) {
             ngx_log_error(NGX_LOG_EMERG, cf->log, 0, 
                     "%s: error reading certificate attributes: \"%*s\"",
                     MODULE_NAME,
